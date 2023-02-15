2b440774cd878efbf8a76456febb7433bb10abf7
taylinswift13 <60133250+taylinswift13@users.noreply.github.com>
2023-02-15 15:10:00 +0100

    Add new folder for assignment

---
+++ Shard/ConsoleApp1/Assignment/GameAssignment.cs
@@ -0,0 +1,34 @@
+﻿using GameAssignment;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Shard
+{
+    class GameAssignment : Game
+    {
+        GameObject top, left, right, bottom;
+        Random rand;
+        public override void update()
+        {
+        }
+
+        public override void initialize()
+        {
+            Player p = new Player();
+            p.Transform.X = 50;
+            p.Transform.Y = 50;
+        }
+
+        public override int getTargetFrameRate()
+        {
+
+            return 60;
+
+
+        }
+    }
+
+
+
+}
---
+++ Shard/ConsoleApp1/Assignment/Player.cs
@@ -0,0 +1,67 @@
+﻿
+using Shard;
+
+namespace GameAssignment
+{
+    class Player : GameObject, InputListener, CollisionHandler
+    {
+        private int health;
+
+        public int Health { get => health; set => health = value; }
+
+        public override void initialize()
+        {
+            this.Transform.SpritePath = Bootstrap.getAssetManager().getAssetPath("Untitled.png");
+            setPhysicsEnabled();
+            MyBody.addRectCollider();
+
+            MyBody.Mass = 1;
+            MyBody.MaxForce = 15;
+            MyBody.Drag = 0f;
+            MyBody.UsesGravity = false;
+            MyBody.StopOnCollision = false;
+            MyBody.ReflectOnCollision = true;
+
+            Transform.Scalex = 2;
+            Transform.Scaley = 2;
+
+            Transform.rotate(90);
+        }
+
+        public void handleInput(InputEvent inp, string eventType)
+        {
+
+        }
+
+
+        public override void update()
+        {
+            Bootstrap.getDisplay().addToDraw(this);
+        }
+
+        public void onCollisionEnter(PhysicsBody x)
+        {
+            Health -= 1;
+
+            if (Health <= 0)
+            {
+                this.ToBeDestroyed = true;
+            }
+        }
+
+        public void onCollisionExit(PhysicsBody x)
+        {
+
+        }
+
+        public void onCollisionStay(PhysicsBody x)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Player: [" + Transform.X + ", " + Transform.Y + ", " + Transform.Wid + ", " + Transform.Ht + "]";
+        }
+
+    }
+}
