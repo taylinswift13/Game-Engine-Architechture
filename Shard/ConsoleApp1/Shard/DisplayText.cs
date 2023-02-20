413ea9fe31c49a729f7dfa89b023fab67787c57d
XYZhv <60133335+XYZhv@users.noreply.github.com>
2023-02-20 16:24:28 +0100

    Update DisplayText.cs

--- Shard/ConsoleApp1/Shard/DisplayText.cs
+++ Shard/ConsoleApp1/Shard/DisplayText.cs
@@ -143,7 +143,7 @@ namespace Shard
         {
             fontLibrary = new Dictionary<string, IntPtr>();
 
-            setSize(1280, 720);
+            setSize(1080, 720);
 
             SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
             SDL_ttf.TTF_Init();
